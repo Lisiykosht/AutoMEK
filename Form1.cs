490ab364c37f5125b409fe957d053c353d3d941f
Kozlov <Kozlov@DESKTOP-MK7JEB9>
2021-04-05 18:25:21 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.IO;
+
+
+
+namespace AutoMEK
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String connectionString = "Server=192.168.2.157;Port=5432;Username=postgresql;Password=sa1512;Database=pg_misc;";
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+            try
+            { npgSqlConnection.Open(); }
+            catch
+            {
+                this.Text += ".... Ошибка подключения к базе!";
+            }
+            finally
+            {
+                this.Text += ".... Соединение установлено!";
+               
+            }
+
+            
+            DirectoryInfo di_pack = new DirectoryInfo(@".\Incoming");
+            DirectoryInfo di_packed = new DirectoryInfo(@".\Outgoing\");
+            if (!di_pack.Exists)
+            { di_pack.Create(); }
+
+            if (!di_packed.Exists)
+            { di_packed.Create(); }
+        }
+    }
+}
