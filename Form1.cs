61e5e932b9e093e5f762cf9b799991ee023bc55e
Kozlov <Kozlov@DESKTOP-MK7JEB9>
2021-04-06 18:30:20 +0300

    Первый фикс

--- Form1.cs
+++ Form1.cs
@@ -11,19 +11,78 @@ using Npgsql;
 using System.Configuration;
 using System.Collections.Specialized;
 using System.IO;
-
+using System.Xml;
 
 
 namespace AutoMEK
 {
+
     public partial class Form1 : Form
     {
-        
+
+        public DirectoryInfo di_pack = new DirectoryInfo(@".\Incoming");
+        public DirectoryInfo di_packed = new DirectoryInfo(@".\Outgoing\");
+        class ZGLV
+        {
+            public string VERSION,  FILENAME, SD_Z, TEST, VER_PO;
+
+            private string _DATA;
+
+           
+            
+            public string DATA
+            { get
+                {
+                    return DATA.ToString();
+                }
+                set
+                {
+                    try
+                    {
+                        DATA = value;
+                    }
+                    catch
+                    {
+                        DATA = "!!!!!";
+                        //Здесь должно добавляться в протокол ошибок
+                    }
+                   
+                    
+                }
+            
+            }
+            
+
+
+
+
+            /* public ZGLV(string version, string data, string filename,string sd_z, string test, string ver_po)
+             {
+                 VERSION = version;
+                 DATA= data; 
+                 FILENAME= filename; 
+                 SD_Z = sd_z;  
+                 TEST = test;
+                 VER_PO = ver_po;
+             }
+            */
+            public override string ToString()
+            {
+                return FILENAME;
+            }
+
+            public void SetField(string name, string val)
+            {
+                var field = typeof(ZGLV).GetField(name);
+                field.SetValue(this, val);
+            }
+        }
+
 
         public Form1()
         {
             InitializeComponent();
-            
+
 
 
         }
@@ -41,17 +100,71 @@ namespace AutoMEK
             finally
             {
                 this.Text += ".... Соединение установлено!";
-               
+
             }
 
-            
-            DirectoryInfo di_pack = new DirectoryInfo(@".\Incoming");
-            DirectoryInfo di_packed = new DirectoryInfo(@".\Outgoing\");
+
+
             if (!di_pack.Exists)
             { di_pack.Create(); }
 
             if (!di_packed.Exists)
             { di_packed.Create(); }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Goer(listBox1);
+        }
+
+        private async void Goer(ListBox listBox)
+        {
+            int i = 0;
+            string str=null;
+            foreach (FileInfo findedFile in di_pack.GetFiles())
+            {
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load(findedFile.FullName);
+
+                XmlElement xRoot = xDoc.DocumentElement;
+ 
+
+        
+              
+
+
+                foreach (XmlNode xnode_1 in xRoot.SelectNodes("ZGLV"))
+                {
+                    ZGLV zerg = new ZGLV();
+
+                    foreach (XmlNode xnode_2 in xnode_1)
+                    {
+                       // listBox.Items.Add(xnode_2.Name + xnode_2.InnerText);
+                        zerg.SetField(xnode_2.Name.ToUpper(), xnode_2.InnerText);
+                      //    str=(xnode_1.Name + " -=- " + xnode_2.Name);
+                        //listBox.Items.Add(str);
+                       
+                    }
+
+                    /*XmlNode attr = xnode.Attributes.GetNamedItem("Version");
+                    if (attr != null)
+                    { listBox1.Items.Add(attr.Value); } else 
+                    {
+                       
+                        
+                        listBox1.Items.Add(i.ToString());
+                        i++;
+                    };*/
+
+                      listBox.Items.Add(zerg.FILENAME);
+                      listBox.Items.Add(zerg.DATA);
+
+                }
+                
+                
+            }
+
         }
     }
 }
